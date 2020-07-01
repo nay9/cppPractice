2a4e85bdf65adc656b56b08754431d0c0d520ad1
nay9 <renenoe94@hotmail.com>
2020-06-30 20:15:44 -0400

    pushing up moar cpp practice

---
+++ moarPractice/moarPractice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int getValueFromUser() {
+    std::cout << "enter an int\n";
+    int input{};
+    std::cin >> input;
+    return input;
+}
+
+int main()
+{
+    int num{ getValueFromUser() };
+
+    std::cout << num << " doubled is: " << num * 2 << '\n';
+
+    return 0;
+}
